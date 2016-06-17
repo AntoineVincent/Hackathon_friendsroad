691868ffc397f79574e076334091dce382a1e3cc
Antoine <anton51200@laposte.net>
2016-06-17 12:30:08 +0200

    update rpouting

--- src/RoadBundle/Resources/public/js/services/userService.js
+++ src/RoadBundle/Resources/public/js/services/userService.js
@@ -4,7 +4,7 @@ function userService($http){
 			return $http.get('app_dev.php/dashboard/get/groupe.json');
 		},
 		create: function(user){
-			return $http.post('/Hackathon_friendsroad/web/app_dev.php/membre/create/1.json', user);
+			return $http.post('app_dev.php/membre/create/1.json', user);
 		}
 	}
 }
